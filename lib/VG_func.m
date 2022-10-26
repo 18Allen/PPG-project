aacc94685c3e6835076b522339821f7da3b42c9d
18Allen <allen891118@gmail.com>
2022-10-27 00:57:01 +0800

    finish VG_func

---
+++ lib/VG_func.m
@@ -0,0 +1,59 @@
+function [deg_mean, deg_std,ast, per_low, per_high,cc_mean, cc_std] = VG_func(RRI)
+
+    %% 
+    % build connection
+    connection_table = zeros([length(RRI),length(RRI)]);
+    for i = 1:length(RRI)-1
+        max_Rslope = -inf; %min_Lslope = inf;
+        for j = i+1:length(RRI)
+            new_Rslope = (RRI(j) - RRI(i))/sum(RRI(i+1:j));
+            if new_Rslope > max_Rslope
+                max_Rslope = new_Rslope;
+                connection_table(i,j) = 1;
+                connection_table(j,i) = 1;
+            else
+               
+            end
+             % Left slope will be covered by previous ones
+        end
+    end
+    
+    % Calculate degree
+    deg = sum(connection_table,2);
+    deg_mean = mean(deg);
+    deg_std = std(deg);
+    %N = N+histcounts(sum(connection_table,2),1:60)/(length(RRI)*length(idx));
+    
+    % assortativity coefficient
+    sum_ab = 0;
+    sum_a2_plus_b2 = 0;
+    sum_a_plus_b = 0;
+    n_edges = 0;
+    for i = 1:length(RRI)-1
+        idx = find(connection_table(i,i+1:end))+i;
+        % deg will always greater than 1
+        if ~isempty(idx) 
+        n_edges = n_edges + length(idx);
+        sum_ab  = sum_ab + sum(deg(i)*deg(idx));
+        sum_a2_plus_b2 = sum_a2_plus_b2 + sum(deg(i)^2 + deg(idx).^2);
+        sum_a_plus_b = sum_a_plus_b + sum(deg(i) + deg(idx));
+        
+        end
+    end
+    M = sum(deg)/2;
+    ast = (M*sum_ab - (sum_a_plus_b/2)^2)/ (M*sum_a2_plus_b2/2  - (sum_a_plus_b/2)^2);
+    % percentage of low/high
+    per_low = sum(deg <= 3)/length(RRI);
+    per_high = sum(deg >= 11)/length(RRI);
+    %%
+    % cluster coefficient
+    cc = zeros(size(RRI(2:end-1)));
+    for i = 2:length(RRI)-1 % sometimes the first/last will have only 1 neighbor
+        idx = find(connection_table(i,:));
+        ki = length(idx);
+        cc(i) = (sum(connection_table(idx,idx),'all'))/(ki*(ki-1));
+    end
+    cc_mean = mean(cc);
+    cc_std = std(cc);
+
+end
--- test_VG_features.m
+++ test_VG_features.m
@@ -19,7 +19,7 @@ PPG_label = cell(size(allsubLabel));
 PPG_label_index = cell(size(allsubLabel));
 
 fs = 200;
-len_epoch = 300; %in second, 5 min
+len_epoch = 270; %in second, 4.5 min
 len_orig = 30;
 
 % basic parameters for STFT 
@@ -34,7 +34,7 @@ upsampling_rate = 500; % For PPG_peak_detection
 
 % Generated feature
 features = cell([n_class,1]);
-n_features = 59; 
+n_features = 6; 
 %1~44 Traditional time,45~49 Traditional freq, 50~52 DFA
 %53 Higuchi fractal dimension, 54 PDFA, 55 ApEn, 56~59 WDFA
 
@@ -43,8 +43,6 @@ n_features = 59;
 %% Parse the data to epoch interval
 IHR_list = cell(size(allsubLabel));
 RRI_list = cell(size(allsubLabel));
-WDFA_list = cell(size(allsubLabel));
-WDFA_curves = cell(size(allsubLabel)); %four combination n and sigma
 s = round(len_epoch/len_orig/2);
 for i =1:N_sub
     i
@@ -64,19 +62,6 @@ for i =1:N_sub
     %TBD temp. In correspond to IHR(end-12:end) =0 in data_parsing.m
     IHR_list{i}(1,1:12) = 1;IHR_list{i}(end,end-12:end) = 1;
     
-    % Get WDFA curve from RRI_res_data
-    RRI_res = RRI_res_data{i};
-    WDFA_curves{i} = cell([4,1]);
-    WDFA_curves{i}{1} = WDFA_fun(RRI_res,30,30,1);
-    WDFA_curves{i}{2} = WDFA_fun(RRI_res,30,90,1);
-    WDFA_curves{i}{3} = WDFA_fun(RRI_res,90,30,1);
-    WDFA_curves{i}{4} = WDFA_fun(RRI_res,90,90,1);
-    
-    WDFA_list{i} = cell([4,1]);
-    for j = 1:4
-        WDFA_list{i}{j} = buffer(WDFA_curves{i}{j},len_epoch,(len_epoch-len_orig))';
-        WDFA_list{i}{j} = WDFA_list{i}{j}(s:end-s+1,:);
-    end
     
     % RRI
     RRI_list{i} = cell(size(PPG_label{i}));
@@ -100,53 +85,14 @@ for l= 1:N_sub
     for m = 1:size(PPG_data{l},1)
         m
         RRI = RRI_list{l}{m};
-                
-    %% Traditional time HRV feature
-    features{l}(m,1:44) = getTraditionalHRVtime(RRI,diff(RRI));
-%     features{l}(m,45) = HFD(RRI,10);
-    features{l}(m,45:49) = getTraditionalHRVfreq(RRI);
-    
-    %% DFA features
-    % select scale for the whole DFA
-    Q = exp(linspace(log(10),log(300),37)); 
-    pts = round(Q);
-    order = 2; % In the paper they use quadratic one
-    F = DFA_fun(RRI,pts,order);
+        min_Rslope = -inf; max_Lslope = inf;
+        % build connection
+        connection_table = zeros([length(RRI),length(RRI)]);
+        for i = 1:length(RRI)
 
-    % A: a 2x1 vector. A(1) is the scaling coefficient "alpha",
-    % A(2) the intercept of the log-log regression, useful for plotting (see examples).
-    A = polyfit(log(pts),log(F)',1);
-    alphaAll = A(1) ;   
-
-    idx = find(Q>=10 & Q<=40) ;
-    A = polyfit(log(pts(idx)),log(F(idx))',1);
-    alpha1 = A(1) ;
-
-    idx = find(Q>=70 & Q<=300) ;
-    A = polyfit(log(pts(idx)),log(F(idx))',1);
-    alpha2 = A(1) ;
-    features{l}(m,50) = alphaAll;
-    features{l}(m,51) = alpha1;
-    features{l}(m,52) = alpha2;
-    
-    %% Higuchi fractal dimension
-    features{l}(m,53) = HFD(RRI,20);
-    
-    %% PDFA
-    P = PDFA(RRI(end-63:end),64,1);
-    p =  polyfit(linspace(0,1,64), P, 1);
-    features{l}(m,54) = p(1);
-    
-    %% ApEn of binary RRI diff in a interval of TBD(right now 5 min)
-    features{l}(m,55) = ApEn(1,0.2*std(diff(RRI) > 0),diff(RRI) > 0,1);
-    %features{l}(m,24) = ApEn(1,0.2 > 0),diff(RRI) > 0,1);
-    
-    %% WDFA feature
-    WDFAs = cell([4,1]);
-    for j = 1:4
-    WDFAs{j} = WDFA_list{l}{j}(m,:);
-    features{l}(m,55+j) = max(WDFAs{j});
-    end
+        end
+        
+        % Calculate degree
     
     end
     save('features&labels.mat','features','PPG_label','PPG_label_index');
