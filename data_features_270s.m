1808fca04f87f0cee0887aac7779a0707729ecc6
18Allen <allen891118@gmail.com>
2022-11-02 16:03:42 +0800

    tmp save on 270s features

--- data_features_270s.m
+++ data_features_270s.m
@@ -34,7 +34,7 @@ upsampling_rate = 500; % For PPG_peak_detection
 
 % Generated feature
 features = cell([n_class,1]);
-n_features = 59;  % TBD
+n_features = 61;  % TBD
 %1~44 Traditional time,45~49 Traditional freq, 50~52 DFA
 %53 Higuchi fractal dimension, 54 PDFA, 55 ApEn, 56~59 WDFA
 
@@ -46,47 +46,48 @@ RRI_list = cell(size(allsubLabel));
 %WDFA_list = cell(size(allsubLabel));
 %WDFA_curves = cell(size(allsubLabel)); %four combination n and sigma
 RRI_res_list = cell(size(allsubLabel));
-s = round(len_epoch/len_orig/2);
+s = round(len_epoch/len_orig);
 for i =1:N_sub
     i
     %PPG
     PPG_data{i} = buffer(allsubPPG{i},fs*len_epoch,fs*(len_epoch-len_orig))';
-    PPG_data{i} = PPG_data{i}(s:end-s+1,:);
+    %PPG_data{i} = PPG_data{i}(s:end,:);
     %Label
-    PPG_label{i} = allsubLabel{i}(s:end-s+1);
+    slabel = ceil(s/2);
+    %PPG_label{i} = allsubLabel{i}(slabel:end-slabel+1);
     %Index (Unused = 0, Used = 1)
     index = ones(size(allsubLabel{i}));
-    index([1:s-1 end-s+2:end]) = 0;
+    index([1:slabel-1 end-slabel+2:end]) = 0;
     PPG_label_index{i} = index;
+    
     %IHR
-    IHR_list{i} = IHR_data{i}(2:end);%Because the original one will have 1 extra second
-    IHR_list{i} = buffer(IHR_list{i},len_epoch*4,(len_epoch-len_orig)*4)';
-    IHR_list{i} = IHR_list{i}(s:end,:);
+    IHR_list{i} = buffer(IHR_data{i},len_epoch*4,(len_epoch-len_orig)*4)';
+    %IHR_list{i} = IHR_list{i}(s:end,:);
     %TBD temp. In correspond to IHR(end-12:end) =0 in data_parsing.m
-    IHR_list{i}(1,1:12) = 1;IHR_list{i}(end,end-12:end) = 1;
+    %IHR_list{i}(1,1:12) = 1;IHR_list{i}(end,end-12:end) = 1;
     
-    % Get WDFA curve from RRI_res_data
-    RRI_res = RRI_res_data{i};
-    WDFA_curves{i} = cell([4,1]);
-    WDFA_curves{i}{1} = WDFA_fun(RRI_res,30,30,1);
-    WDFA_curves{i}{2} = WDFA_fun(RRI_res,30,90,1);
-    WDFA_curves{i}{3} = WDFA_fun(RRI_res,90,30,1);
-    WDFA_curves{i}{4} = WDFA_fun(RRI_res,90,90,1);
-    
-    WDFA_list{i} = cell([4,1]);
-    for j = 1:4
-        WDFA_list{i}{j} = buffer(WDFA_curves{i}{j},len_epoch,(len_epoch-len_orig))';
-        WDFA_list{i}{j} = WDFA_list{i}{j}(s:end-s+1,:);
-    end
+%     % Get WDFA curve from RRI_res_data
+     RRI_res = RRI_res_data{i};
+%     WDFA_curves{i} = cell([4,1]);
+%     WDFA_curves{i}{1} = WDFA_fun(RRI_res,30,30,1);
+%     WDFA_curves{i}{2} = WDFA_fun(RRI_res,30,90,1);
+%     WDFA_curves{i}{3} = WDFA_fun(RRI_res,90,30,1);
+%     WDFA_curves{i}{4} = WDFA_fun(RRI_res,90,90,1);
+%     
+%     WDFA_list{i} = cell([4,1]);
+%     for j = 1:4
+%         WDFA_list{i}{j} = buffer(WDFA_curves{i}{j},len_epoch,(len_epoch-len_orig))';
+%         WDFA_list{i}{j} = WDFA_list{i}{j}(s:end,:);
+%     end
     
     % RRI_res_list for teager energy
     RRI_res_list{i} = buffer(RRI_res,len_epoch,(len_epoch-len_orig))';
-    RRI_res_list{i} = RRI_res_list{i}(s:end-s+1,:);
+    %RRI_res_list{i} = RRI_res_list{i}(s:end,:);
     
     % RRI
     RRI_list{i} = cell(size(PPG_label{i}));
     for j = 1:length(PPG_label{i})
-        idx = find(locs_data{i} > (j-1)*len_orig*upsampling_rate & locs_data{i} <= (len_epoch+(j-1)*len_orig)*upsampling_rate);
+        idx = find(locs_data{i} > (-(len_epoch-len_orig)/2 + (j-1)*len_orig)*upsampling_rate & locs_data{i} <= ((len_epoch-len_orig)/2+(j)*len_orig)*upsampling_rate);
         RRI_list{i}{j} = RRI_data{i}(idx(1:end-1));
         
         % Apply change if less than 300 RRI in an epoch
@@ -100,11 +101,12 @@ end
 
 %%  Analysis
 for l= 1:N_sub
-    features{l} = zeros([size(PPG_data{l},1),n_features]);
+    features{l} = zeros([size(allsubLabel{l},1),n_features]);
     
-    for m = 1:size(PPG_data{l},1)
+    for m = find(PPG_label_index{l} > 0)
         m
         RRI = RRI_list{l}{m};
+        RRI_res = RRI_res_list{l}(m,:);
                 
     %% Traditional time HRV feature
     features{l}(m,1:44) = getTraditionalHRVtime(RRI,diff(RRI));
@@ -147,13 +149,15 @@ for l= 1:N_sub
     %features{l}(m,24) = ApEn(1,0.2 > 0),diff(RRI) > 0,1);
     
     %% WDFA feature
-    WDFAs = cell([4,1]);
-    for j = 1:4
-    WDFAs{j} = WDFA_list{l}{j}(m,:);
-    features{l}(m,55+j) = max(WDFAs{j});
-    end
-    
-    
+%     WDFAs = cell([4,1]);
+%     for j = 1:4
+%     WDFAs{j} = WDFA_list{l}{j}(m,:);
+%     features{l}(m,55+j) = max(WDFAs{j});
+%     end
+    %% Teager energy
+    te = teager_energy_func(RRI_res);
+    features{l}(m,60) = mean(te);
+    features{l}(m,61) = std(te);
     end
     save('features&labels.mat','features','PPG_label','PPG_label_index');
 
