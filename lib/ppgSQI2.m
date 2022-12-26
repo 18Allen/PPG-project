e141a99bc9bb26f39bdfe789d4c6a4e60ba306e4
WayneTheOdds <108716642+WayneTheOdds@users.noreply.github.com>
2022-12-26 22:02:19 +0800

    Create ppgSQI2

--- lib/ppgSQI2.m
+++ lib/ppgSQI2.m
@@ -1,4 +1,4 @@
-function [val] = ppgSQI(cut_signal, sampling_rate)
+function [val] = ppgSQI2(cut_signal, sampling_rate)
 
 % resamples to 20 Hz from 100 Hz
 fs = 20;
@@ -50,20 +50,16 @@ pks(3) = band(1) + Mm(1) - 1;
 % around largest value M on respiratory frequency band of .1-.75Hz.
 % Mm(1) to take location of first peak if there are more than one.
 % MPA = power(.5*sec+Mm(1)-1) + power(.5*sec+Mm(1)-2) + power(.5*sec+Mm(1));
-MPA = sum(power(pks(1)-1:pks(1)+1));
-for h = 2:3
-    MPA = MPA + sum(power(pks(h)-1:pks(h)+1));
-end
+MPA1 = sum(power(pks(1)-1:pks(1)+1));
+MPA2 = sum(power(pks(2)-1:pks(2)+1));
 
 % calculate total respiratory range (TRA)
 % sum of all values within respiratory range of .1Hz-.75Hz
-TRA = sum(power(.1*sec:pks(1)+round(.5*sec)));
-for h = 2:3
-    TRA = TRA + sum(power(pks(h)-round(0.5*sec):pks(h)+round(0.5*sec)));
-end
+TRA1 = sum(power(.1*sec:pks(1)+round(.5*sec)));
+TRA2 = sum(power(pks(2)-round(0.5*sec):pks(2)+round(0.5*sec)));
 
 % FFT-RQI is ratio of MPA to TRA
-val = MPA/TRA;
+val = mean([MPA1/TRA1 MPA2/TRA2]);
 
 % assign RQI value into RQIs cell array
 % QIs(i,:) = QI_val;
